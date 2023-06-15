56a58d3192602e91cf2c64c209bf74f99dbddbb3
dianingty-tkp <dian.ayuningtyas@tokopedia.com>
2023-06-15 04:43:38 +0000

    add disease details

--- plant_disease_detection/models.py
+++ plant_disease_detection/models.py
@@ -4,14 +4,17 @@ from tensorflow.keras import layers
 import numpy as np
 import matplotlib.pyplot as plt
 import json
+import pandas as pd
 
 
 class DiseasePrediction:
     def __init__(self,
                 model_path="20230612_01/plant_disease",
-                class_indices_path="class_indices.json"):
+                class_indices_path="class_indices.json",
+                disease_details_path="disease_details.csv"):
         self.model_path = model_path
         self.class_indices_path = class_indices_path
+        self.disease_details_path = disease_details_path
         
         # load model
         self.model = tf.keras.models.load_model(self.model_path)
@@ -19,6 +22,9 @@ class DiseasePrediction:
         # import indices
         with open(self.class_indices_path, "r") as f:
             self.class_indices = json.load(f)
+            
+        # import disease details
+        self.disease_details = pd.read_csv(self.disease_details_path)
             
     def predict(self, 
                 image_path,
@@ -29,8 +35,11 @@ class DiseasePrediction:
         input_arr = tf.expand_dims(tf.keras.utils.img_to_array(image), axis=0)
 
         # predict image
-        prediction = self.model.predict(input_arr)
+        prediction = self.model.predict(input_arr, verbose=False)
         prediction_idx = np.argmax(prediction)
 
         # get the value
-        return self.class_indices[str(prediction_idx)]
+        disease = self.class_indices[str(prediction_idx)]
+        result = self.disease_details[self.disease_details["disease"] == disease]
+        result = result["details"].tolist()[0]
+        print(result)
